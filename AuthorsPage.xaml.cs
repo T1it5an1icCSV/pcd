2c7f2f9455ba43779b8d1195f492ee44d93224cc
T1it5an1icCSV <145016510+T1it5an1icCSV@users.noreply.github.com>
2024-04-13 15:05:42 +0300

    Add files via upload

---
+++ AuthorsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Practica_EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica_EF
+{
+    /// <summary>
+    /// Логика взаимодействия для Books.xaml
+    /// </summary>
+    public partial class AuthorsPage : Page
+    {
+        private Practica1Entities3 context = new Practica1Entities3();
+
+        public AuthorsPage()
+        {
+            InitializeComponent();
+            Practica1DataGrid.ItemsSource = context.Authors.ToList();
+        }
+
+    }
+}
+
